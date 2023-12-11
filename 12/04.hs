aa52f776a0adb5925dd6621300072590b1331fb5
YawKar <yawkarpov@gmail.com>
2023-12-11 16:52:07 +0000

    12 again! !

--- 12/04.hs
+++ 12/04.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 import Data.Function (on)
-import Data.List (elemIndex)
+import Data.List (elemIndex, nub)
 
 infixl 4 :@
 
@@ -30,11 +30,17 @@ newtype Env = Env [(Symb, Type)]
 newtype SubsTy = SubsTy [(Symb, Type)]
   deriving (Eq, Show)
 
+instance Semigroup SubsTy where
+  (<>) = composeSubsTy
+
+instance Monoid SubsTy where
+  mempty = SubsTy []
+
 appSubsTy :: SubsTy -> Type -> Type
 appSubsTy (SubsTy sst) t@(TVar s)
   | Just i <- s `elemIndex` map fst sst = snd $ sst !! i
   | otherwise = t
 appSubsTy sst (t1 :-> t2) = ((:->) `on` appSubsTy sst) t1 t2
 
-appSubsEnv :: SubsTy -> Env -> Env
-appSubsEnv sst (Env env) = Env [(s, appSubsTy sst t) | (s, t) <- env]
+composeSubsTy :: SubsTy -> SubsTy -> SubsTy
+composeSubsTy sst1'@(SubsTy sst1) (SubsTy sst2) = SubsTy $ nub $ [(s, appSubsTy sst1' t) | (s, t) <- sst2] <> sst1
